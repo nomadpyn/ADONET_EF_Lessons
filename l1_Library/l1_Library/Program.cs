0bee346b261f3eae1205fdb776a965c0f6c36244
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-02-16 15:18:17 +0700

    Commit 6
    l1_Library
    Add comments

--- l1_Library/l1_Library/Program.cs
+++ l1_Library/l1_Library/Program.cs
@@ -10,21 +10,22 @@ static void addAuthorToDb(string connString)
 {
     try
     {
+// открываем соединение с базой данных в присоединенном режиме
         SqlConnection conn = new SqlConnection(connString);
         conn.Open();
-
+// запрашиваем у пользователя данные для внесения в таблицу
         Console.WriteLine("Введите имя автора");
         string name = Console.ReadLine();
         Console.WriteLine("Введите фамилию автора");
         string fname = Console.ReadLine();
-
+// создаем параметры для безопасного ввода данных в таблицу
         SqlParameter pName = new SqlParameter("@FirstName", SqlDbType.NVarChar);
         pName.Value = name;
         SqlParameter pFname = new SqlParameter("@LastName", SqlDbType.NVarChar);
         pFname.Value = fname;
-
+// строка запроса SQL
         string insertString = "insert into Authors (FirstName, LastName) Values (@FirstName,@LastName)";
-
+// выполняем команду и закрываем все подключения
         SqlCommand cmd = new SqlCommand(insertString,conn);
         cmd.Parameters.AddRange(new SqlParameter[] { pName, pFname });
         cmd.ExecuteNonQuery();
@@ -41,9 +42,10 @@ static void addBookToDb(string connString)
 {
     try
     {
+// открываем соединение с базой данных в присоединенном режиме
         SqlConnection conn = new SqlConnection(connString);
         conn.Open();
-
+// узнаем сколько авторов у нас в базе и запрашиваем ввод у пользователя с проверкой на корректность данных
         SqlCommand chckAthrs = new SqlCommand("select * from Authors", conn);
         SqlDataReader sqlAthrs = chckAthrs.ExecuteReader();
         int line = 0;
@@ -58,6 +60,7 @@ static void addBookToDb(string connString)
         Console.WriteLine($"У вас базе {line} авторов. Выберите автора в данном диапазоне посредством ввода цифры");
         string choise = Console.ReadLine();
         int index;
+// проверяем данных на корректность
         if(int.TryParse(choise, out index))
         {
             if (!indexes.Contains(index)) 
@@ -70,13 +73,14 @@ static void addBookToDb(string connString)
         {
             index = indexes[0];
         }
+// запрашиваем данные у пользователя для ввода в базу данных
         Console.WriteLine("Введите название книги");
         string title = Console.ReadLine();
         Console.WriteLine("Введите стоимость книги");
         int price = Int32.Parse(Console.ReadLine());
         Console.WriteLine("Введите количество страниц");
         int pages = Int32.Parse(Console.ReadLine());
-
+// создаем параметры для безопасного ввода данных в таблицу
         SqlParameter pAuthor = new SqlParameter("@AuthorId", SqlDbType.Int);
         pAuthor.Value = index;
         SqlParameter pTitle = new SqlParameter("@Title", SqlDbType.NVarChar);
@@ -85,10 +89,10 @@ static void addBookToDb(string connString)
         pPrice.Value = price;
         SqlParameter pPages = new SqlParameter("@Pages", SqlDbType.Int);
         pPages.Value = pages;
-
+// создаем строку SQL запрсоа
         string insertString = "insert into Books (AuthorId, Title,Price,Pages) " +
             "Values (@AuthorId,@Title,@Price,@Pages)";
-
+// выполняем команду и закрываем все подключения
         SqlCommand cmd = new SqlCommand(insertString, conn);
         cmd.Parameters.AddRange(new SqlParameter[] { pAuthor,pTitle,pPrice,pPages});
         cmd.ExecuteNonQuery();
@@ -104,17 +108,18 @@ static void addBookToDb(string connString)
 static void getCountAthrsAndBks(string connString)
 {
     try 
-    { 
+    {
+// открываем соединение с базой данных в присоединенном режиме
         SqlConnection conn = new SqlConnection(connString);
         conn.Open();
 
         SqlCommand cmd = new SqlCommand(connString, conn);
-
+// создаем команду для запросов, которые вернут нам скалярное значение
         cmd.CommandText = "Select COUNT (id) From Authors";
         var countAthrs = cmd.ExecuteScalar();
         cmd.CommandText = "Select COUNT (id) From Books";
         var countBks = cmd.ExecuteScalar();
-
+// вывод полученных данных на экран
         Console.WriteLine($"В базе данных авторов: {countAthrs}");
         Console.WriteLine($"В базе данных книг: {countBks}");
 
@@ -130,16 +135,17 @@ static void calcSumBooks(string connString)
 {
     try 
     {
+// открываем соединение с базой данных в присоединенном режиме
         SqlConnection conn = new SqlConnection(connString);
         conn.Open();
-
+// переменные для хранения данных о всех суммах
         int sumPrice = 0, sumPages = 0;
-
+// создаем строку запроса SQL
         string getString = "Select Title,Price,Pages From Books";
         SqlCommand cmd = new SqlCommand(getString, conn);
         
         SqlDataReader reader = cmd.ExecuteReader();
-
+// пока есть данные для считывание работаем с полученными данными
         while (reader.Read())
         {
             Console.WriteLine($"Книга : {reader["Title"]}");
