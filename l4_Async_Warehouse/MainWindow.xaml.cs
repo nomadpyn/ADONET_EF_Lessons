e37ccc502b9555ca44df692ff6e8c10800577418
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-02-27 14:01:18 +0700

    Commit 31
    l4_Async_Warehouse
    add async getDataFromDB
    add async startButton_Click

--- l4_Async_Warehouse/MainWindow.xaml.cs
+++ l4_Async_Warehouse/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace l4_Async_Warehouse
 {
@@ -20,9 +22,65 @@ namespace l4_Async_Warehouse
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string connString= @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Warehouse; Integrated Security = true";
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var task1= getDataFromDB(connString, "select * from Providers");
+            var task2 = getDataFromDB(connString, "select * from ProductType");
+
+            gridProviders.ItemsSource =(await task1).DefaultView;
+            gridProductTypes.ItemsSource = (await task2).DefaultView;          
+        }
+        private static async Task<DataTable> getDataFromDB(string connString, string sqlQuery)
+        {
+            SqlConnection connection = new SqlConnection(connString);
+
+            await connection.OpenAsync();
+
+            //MessageBox.Show($"{sqlQuery} подключение создано");
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, connection);
+
+            DataTable table = new DataTable();
+
+            using (SqlDataReader reader = await cmd.ExecuteReaderAsync()) 
+            {
+                int line = 0;
+                do
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i <
+                            reader.FieldCount; i++)
+                            {
+                                table.Columns.Add(reader.
+                                GetName(i));
+                            }
+                            line++;
+                        }
+                        DataRow row = table.NewRow();
+                        for (int i = 0; i < reader.
+                        FieldCount; i++)
+                        {
+                            row[i] = await reader.
+                            GetFieldValueAsync<Object>(i);
+                        }
+                        table.Rows.Add(row);
+                    }
+                } while (reader.NextResult());
+            }
+
+            //MessageBox.Show($"{sqlQuery} подключение закрыто");
+
+            return table;
+        }        
+    }    
 }
