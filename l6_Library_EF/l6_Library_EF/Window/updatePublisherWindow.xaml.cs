788c5eaa5fdd0dfb8090cca90e95e81114e84a43
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-03-14 10:56:36 +0700

    Commit 54
    l6_Library_EF:
    move Windows to folder "Window"

---
+++ l6_Library_EF/l6_Library_EF/Window/updatePublisherWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace l6_Library_EF
+{
+    /// <summary>
+    /// Interaction logic for updatePublisherWindow.xaml
+    /// </summary>
+    public partial class updatePublisherWindow : Window
+    {
+        Publisher updated = new Publisher();
+        public updatePublisherWindow()
+        {
+            InitializeComponent();
+            using (LibraryEntities db = new LibraryEntities())
+            {
+                var data = db.Publisher.ToList();
+
+                publishersBox.ItemsSource = data;
+            }
+        }
+
+        private void publishersBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updated.Id = (int)publishersBox.SelectedValue;
+        }
+
+        private void updatePublisher_Click(object sender, RoutedEventArgs e)
+        {
+            updated.PublisherName = publisherName.Text;
+            updated.Address = publisherAdress.Text;
+            ViewModel.updPublisher(updated);
+            this.Close();
+        }
+    }
+}
