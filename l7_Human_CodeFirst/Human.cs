85af32da6988d095fa9597968661c060a71ae510
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-03-15 15:17:33 +0700

    Commit 63
    l7_Human_CodeFirst:
    - create app.conifg;
    - create class Human

---
+++ l7_Human_CodeFirst/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7_Human_CodeFirst
+{
+    public class Human
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public string Fname { get; set; }
+        public string Gender { get; set; }  
+        public int Age { get; set; }
+
+        public Human( string name, string fname, string gender, int age)
+        {
+            Name = name;
+            Fname = fname;
+            Gender = gender;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return this.Fname + " " + this.Name + "\nПол: " + this.Gender
+                + " Возраст " + this.Age;
+        }
+    }
+}
