6786a0e4353381a00ebb96fdacd8208bcee3a64c
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-03-15 17:29:13 +0700

    Commit 65
    l7_Human_CodeFirst:
    add class Func and methods:
    - create Human;
    - add Human;
    - show Humas;

---
+++ l7_Human_CodeFirst/Func.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7_Human_CodeFirst
+{
+    public class Func
+    {
+
+        public static void addHuman()
+        {
+            try
+            {
+                using (HumanContext db = new HumanContext())
+                {
+                    db.Humans.Add(createHuman());
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void showHumans()
+        {
+            try
+            {
+                using (HumanContext db = new HumanContext())
+                {
+                    var humans = db.Humans.ToList();
+
+                    foreach (Human human in humans)
+                    {
+                        Console.WriteLine(human + "\n");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static Human createHuman()
+        {
+            Console.WriteLine("Введите фамилию человека");
+            string? fname = Console.ReadLine();
+            Console.WriteLine("Введите имя человека");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Введите возраст человека");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите пол человека");
+            Console.WriteLine("М - Мужской, Ж - Женский");
+            char choise = Char.Parse(Console.ReadLine());
+            string gender = "не указан";
+            if (choise == 'М')
+            {
+                gender = "Мужской";
+            }
+            if (choise == 'Ж')
+            {
+                gender = "Женский";
+            }
+
+            return new Human(name, fname, gender, age);
+        }
+    }
+}
---
+++ l7_Human_CodeFirst/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using l7_Human_CodeFirst;
+
+
+Func.addHuman();
+Func.showHumans();
