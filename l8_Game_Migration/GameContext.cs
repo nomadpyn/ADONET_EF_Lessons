e3c5c28e6f08ffe973e25ac094229835b321b9a4
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-03-17 12:30:32 +0700

    Commit 69
    l8_Game_Migration:
    create class Game;
    create class GameContext;
    create Game.config

---
+++ l8_Game_Migration/GameContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace l8_Game_Migration
+{
+    public class GameContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["GameDB"].ConnectionString);
+        }
+
+        public DbSet<Game> Games { get; set;}
+    }
+}
