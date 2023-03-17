8de7ce94a85cefed978d49ced3996429a1e3ab3f
Alexey <105908089+nomadpyn@users.noreply.github.com>
2023-03-17 14:10:47 +0700

    Commit 71
    create l8_GameMigr:
    create class Game;
    create class GmContext;
    create local README

---
+++ l8_GameMigr/GmContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l8_GameMigr
+{
+    public class GmContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+        public GmContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB ;Initial Catalog= GameShop;Integrated Security=True");
+        }
+    }
+}
